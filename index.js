5c09bca9b2d9e83ae9644f0b8c74c295f643a2c1
lucasfigueired0 <lucasfigrock@gmail.com>
2023-12-10 23:11:50 -0300

    feat: finished endpoints

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const { Client } = require('pg');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const postgre = require('pg');
 const { chave } = require('./generateKey');
 require('dotenv').config();
@@ -70,54 +70,64 @@ const execSQLQueryUser = (sqlQry, res) => {
     });
 }
 
-// Rota para visualizar todos os usuários
-// app.get('/usuarios', (req, res) => {
-//     execSQLQuery("SELECT * FROM usuario", res)
-// });
 
-//Rota para achar usuário com base no id
-// app.get('/usuarios/:id?', (req, res) => {
-//     let filter = '';
-//     if (req.params.id) filter = ' WHERE id=' + parseInt(req.params.id);
-//     execSQLQuery('SELECT * FROM usuario' + filter, res.status("Okay"));
-// })
-
-//Excluindo um usuário
-app.delete('/usuarios/:id?', (req, res) => {
-    execSQLQuery('DELETE FROM usuario WHERE id=' + parseInt(req.params.id), res);
-})
-
-//Rota para adicionar um usuário
-app.post('/usuarios/sign-up', (req, res) => {
+app.post('/users/sign-up', (req, res) => {
     const id = chave();
     console.log(id)
-    const nome = req.body.nome.substring(0, 90);
-    const email = req.body.email.substring(0, 90);
-    const senha = req.body.senha.substring(0, 30);
-    execSQLQuery(`insert into usuario(id, nome, email, senha) 
-    values ('${id}','${nome}', '${email}','${senha}')`, res)
+    const name = req.body.name.substring(0, 100);
+    const last_name = req.body.last_name.substring(0, 100);
+    const username = req.body.username.substring(0, 100);
+    const email = req.body.email.substring(0, 100);
+    const registration_number = req.body.registration_number.substring(0, 100);
+    const undergraduate_degree = req.body.undergraduate_degree.substring(0, 100);
+    const initial_semester = req.body.initial_semester.substring(0, 100);
+    const level = req.body.level.substring(0, 100);
+    const password = req.body.password.substring(0, 100);
+    const cpf = req.body.cpf.substring(0, 100);
+    const rg = req.body.rg.substring(0, 30);
+    const date_of_birth = req.body.date_of_birth.substring(0, 100);
+
+    execSQLQuery(`insert into student(name,
+                                      last_name, 
+                                      email,
+                                      username,
+                                      registration_number,
+                                      cpf,
+                                      rg,
+                                      date_of_birth,
+                                      undergraduate_degree,
+                                      initial_semester,
+                                      level,
+                                      password
+                                      ) 
+    values ('${name}',
+            '${last_name}',
+            '${email}',
+            '${username}',
+            '${registration_number}',
+            '${cpf}',
+            '${rg}',
+            '${date_of_birth}',
+            '${undergraduate_degree}',
+            '${initial_semester}',
+            '${level}',
+            '${password}')`, res)
 })
 
-//Rota para logar usuário
-app.post('/usuarios/sign-in', (req, res) => {
-    const email = req.body.email.substring(0, 90);
-    const senha = req.body.senha.substring(0,30);
-    execSQLQueryUser(`SELECT id, nome, email FROM usuario WHERE email='${email}' AND senha='${senha}' `, res);
+
+app.post('/users/sign-in', (req, res) => {
+    const username = req.body.username.substring(0, 90);
+    const password = req.body.password.substring(0,30);
+    execSQLQueryUser(`SELECT id, 
+                      name,
+                      last_name,
+                      email,
+                      registration_number,
+                      undergraduate_degree,
+                      initial_semester,
+                      level FROM student WHERE username='${username}' AND password='${password}' `, res);
 })
 
-//Atualiza usuário
-app.patch('/usuarios/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const nome = req.body.nome.substring(0, 90);
-    const email = req.body.email.substring(0, 90);
-    const senha = req.body.senha.substring(0, 30);
 
-    execSQLQuery(`UPDATE usuario SET 
-    nome='${nome}', 
-    email='${email}', 
-    senha='${senha}' WHERE id = ${id}`, res)
-})
-
-//Inicia o servidor
 app.listen(port);
 console.log('Api funcionando')
